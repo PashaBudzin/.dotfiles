c728c618093f9aa7a5bf4aa0950b2355658ecd1d
PashaBudzin <pashabudzin@proton.me>
2025-03-26 19:15:36 +0200

    set default browser

--- home/apps.nix
+++ home/apps.nix
@@ -4,4 +4,13 @@
     materialgram
     mangohud
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+    };
+  };
 }
