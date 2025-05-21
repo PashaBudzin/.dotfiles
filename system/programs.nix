0c50eeeece8dc555fb698f4b2cd90ce27c7787f1
PashaBudzin <pashabudzin@proton.me>
2025-05-21 17:59:04 +0300

    add port 4321 as exception to firewall

--- system/programs.nix
+++ system/programs.nix
@@ -2,6 +2,6 @@
   environment.defaultPackages = with pkgs; [ bottles ffmpeg imagemagick jq ];
 
   services.zerotierone.enable = true;
-  networking.firewall.allowedUDPPorts = [ 4230 ];
-  networking.firewall.allowedTCPPorts = [ 4230 ];
+  networking.firewall.allowedUDPPorts = [ 4230 4321 ];
+  networking.firewall.allowedTCPPorts = [ 4230 4321 ];
 }
