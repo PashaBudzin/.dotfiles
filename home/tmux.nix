9f4cc3b8030ff18e01b487a8a8bced7bbeedfd6b
PashaBudzin <pashabudzin@proton.me>
2025-06-19 22:57:15 +0300

    update flake

--- home/tmux.nix
+++ home/tmux.nix
@@ -32,15 +32,15 @@
       bind-key k select-pane -U
       bind-key l select-pane -R
 
-      set -g @plugin 'christoomey/vim-tmux-navigator'
-      set -g @plugin 'tmux-plugins/tmux-yank'
+      # set -g @plugin 'christoomey/vim-tmux-navigator'
+      # set -g @plugin 'tmux-plugins/tmux-yank'
+      # set -g @plugin 'catppuccin/tmux'
 
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
       bind-key -T copy-mpde-vi y send-keys -X copy-selection-and-cancel
       set -g default-terminal "tmux-256color"
       set -ag terminal-overrides ",xterm-256color:RGB"
-      set -g @plugin 'catppuccin/tmux'
       set -g @catppuccin_flavour 'mocha'
       set -g @catppuccin_window_left_separator ""
       set -g @catppuccin_window_right_separator ""
