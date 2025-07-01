49825793d9fc96b216696ba8b755fb3c71e1cee1
PashaBudzin <pashabudzin@proton.me>
2025-07-01 18:06:22 +0300

    add pied

--- home/apps.nix
+++ home/apps.nix
@@ -4,8 +4,23 @@
     materialgram
     mangohud
     libresprite
+    pied
     tdf
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/xhtml+xml" = "zen-beta.desktop";
+      "text/html" = "zen-beta.desktop";
+      "text/xml" = "zen-beta.desktop";
+      "x-scheme-handler/ftp" = "zen-beta.desktop";
+      "x-scheme-handler/http" = "zen-beta.desktop";
+      "x-scheme-handler/https" = "zen-beta.desktop";
+    };
+  };
+
+  home.sessionVariables = { BROWSER = "zen-browser"; };
 
   programs.zen-browser.enable = true;
 }
