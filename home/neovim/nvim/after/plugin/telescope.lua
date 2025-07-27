f023d464de4364cb2797834c464c5f9287a6a967
PashaBudzin <pashabudzin@proton.me>
2025-07-27 23:13:39 +0300

    update telescope

--- home/neovim/nvim/after/plugin/telescope.lua
+++ home/neovim/nvim/after/plugin/telescope.lua
@@ -1,7 +1,17 @@
 local builtin = require("telescope.builtin")
 
--- require("telescope").setup({})
--- require("telescope").load_extension("fzf")
+require("telescope").setup({
+	defaults = {
+		previewer = false,
+		-- Try forcing a normal buffer
+		buffer_previewer_maker = function(filepath, bufnr, opts)
+			opts.use_ft_detect = true
+			require("telescope.previewers").buffer_previewer_maker(filepath, bufnr, opts)
+		end,
+	},
+})
+
+require("telescope").load_extension("fzf")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
