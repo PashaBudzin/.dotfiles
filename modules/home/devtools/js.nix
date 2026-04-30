eaf6b3a26d91c518dd96a18517bcf2e844a0d891
PashaBudzin <pashabudzin@proton.me>
2026-04-30 12:10:03 +0300

    feat: add oxfmt/lint

--- modules/home/devtools/js.nix
+++ modules/home/devtools/js.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   home.packages = with pkgs; [
     bun
     typescript-language-server
 
     prettierd
+
+    oxfmt
+    oxlint
 
     tailwindcss-language-server
     astro-language-server
